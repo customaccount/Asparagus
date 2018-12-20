b8f7395755e9ef15904156a9e1bfb7b2e3a0228e
BondarIvan <IvanBondar@coherentsolutions.com>
2018-12-20 14:02:31 +0300

    added core 2.2 WebApi and WebJob for hub device

---
+++ AzureTraining/AzureTraining.WebJob.Hub/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using WebJobs.Extensions.RabbitMQ.Attributes;
+
+namespace AzureTraining.WebJob.Hub
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an RabbitMq Queue.
+        public static void ProcessQueueMessage(
+            //[RabbitQueueBinder("", "test")]
+            [RabbitQueueTrigger("test")]
+            string message, TextWriter log)
+        {
+            //log.WriteLine(message);
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ AzureTraining/AzureTraining.WebJob.Hub/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using WebJobs.Extensions.RabbitMQ.Config;
+
+namespace AzureTraining.WebJob.Hub
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .UseEnvironment("Development")
+                .ConfigureWebJobs(b =>
+                {
+                    //b.AddCosmosDB();
+                    b.AddRabbitMq();;
+                });
+
+            var host = builder.Build();
+
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
