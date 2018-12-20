de04afc855a6b4e9b29dcbd3c49ba1c3d1138333
BondarIvan <IvanBondar@coherentsolutions.com>
2018-12-20 19:11:37 +0300

    added webjob for climate device.

---
+++ AzureTraining/AzureTraining.WebJob.ClimateControlDevice/Functions.cs
@@ -0,0 +1,16 @@
+﻿using WebJobs.Extensions.RabbitMQ.Attributes;
+
+namespace AzureTraining.WebJob.ClimateControlDevice
+{
+    public class Functions
+    {
+        //public static void ProcessQueueMessage(
+        //    [RabbitQueueTrigger("hello")] string message)
+        //{}
+
+        public static void SendQueueMessage([RabbitMessage("", "hello")] out string message)
+        {
+            message = "message from climate device";
+        }
+    }
+}
---
+++ AzureTraining/AzureTraining.WebJob.ClimateControlDevice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using WebJobs.Extensions.RabbitMQ.Config;
+
+namespace AzureTraining.WebJob.ClimateControlDevice
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .UseEnvironment("Development")
+                .ConfigureWebJobs(b =>
+                {
+                    //b.AddCosmosDB();
+                    b.AddRabbitMq();
+                });
+
+            using (var host = builder.Build())
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
