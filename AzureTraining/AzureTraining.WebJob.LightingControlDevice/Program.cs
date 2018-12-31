a945e96f653db2604aed967f2c345d70a7dc98dd
BondarIvan <IvanBondar@coherentsolutions.com>
2018-12-31 14:45:03 +0300

    added webjobs for HumidifierControlDevice and LightingControlDevice

---
+++ AzureTraining/AzureTraining.WebJob.LightingControlDevice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using AzureTraining.DeviceEmulators.Abstractions;
+using AzureTraining.DeviceEmulators.Abstractions.Factory;
+using AzureTraining.DeviceEmulators.Abstractions.ServiceInterfaces;
+using AzureTraining.DeviceEmulators.Devices.Model;
+using AzureTraining.DeviceEmulators.Factory;
+using AzureTraining.DeviceEmulators.Repositories;
+using AzureTraining.DeviceEmulators.ServiceImplementations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebJobs.Extensions.RabbitMQ.Config;
+
+namespace AzureTraining.WebJob.LightingControlDevice
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .UseEnvironment("Development")
+                .ConfigureWebJobs(b =>
+                {
+                    //b.AddCosmosDB();
+                    b.AddRabbitMq();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<IRepository<DeviceItem>, DeviceRepository<DeviceItem>>();
+                    services.AddSingleton<IDeviceManager, DeviceManager>();
+                    services.AddSingleton<IDeviceFactory, DeviceFactory>();
+                    services.AddSingleton<ILogger, ConsoleLogger>();
+                })
+                .UseConsoleLifetime();
+
+            using (var host = builder.Build())
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
